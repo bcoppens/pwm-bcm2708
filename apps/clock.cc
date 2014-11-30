360966a1d1c772a18449cf4adbea507f6873c0b4
Bart Coppens <kde@bartcoppens.be>
2014-11-30 21:47:27 +0100

    Add hours, minutes and seconds to the LED display

--- apps/clock.cc
+++ apps/clock.cc
@@ -1,39 +1,108 @@
 #include <cmath>
 #include <chrono>
 #include <cstdlib>
+#include <iostream>
 #include <thread>
+
+#include <time.h>
 
 #include "pwm_bcm2708.h"
 #include "ws2812_bcm2708.h"
 
 using namespace std;
+using namespace chrono;
 
 static ws2812_array clean_colors;
 
 const int ring_size = 16;
 const int ms_per_frame = 125;
-const int frames_per_min = 60*1000/ms_per_frame;
+const int max_brightness = 140;
 
 static void output_list(struct ws2812_array* list, struct pwm_bwm2708_dev* pwm, struct ws28128_bcm2708* out) {
   ws28128_bcm2708_render(list, out);
   pwm_bwm2708_write_data(pwm, out->buffer, out->size);
 }
 
-static ws2812_color color_fraction(const ws2812_color& color, float fraction) {
-  return { uint8_t(color.red * fraction),
-           uint8_t(color.green * fraction),
-           uint8_t(color.blue * fraction) };
+template<typename T>
+static inline T clamp(T value, T min, T max) {
+  if (value > max)
+    return max;
+  if (value < min)
+    return min;
+  return value;
 }
 
-static void insert_blended_at(ws2812_array& list, const ws2812_color& color, float pos) {
+static ws2812_color color_add(const ws2812_color& c1, const ws2812_color& c2) {
+  return { uint8_t(clamp(c1.red + c2.red,     0, max_brightness)),
+           uint8_t(clamp(c1.green + c2.green, 0, max_brightness)),
+           uint8_t(clamp(c1.blue + c2.blue,   0, max_brightness)) };
+}
+
+static ws2812_color color_fraction(const ws2812_color& color, float fraction) {
+  return { uint8_t(color.red   * fraction),
+           uint8_t(color.green * fraction),
+           uint8_t(color.blue  * fraction) };
+}
+
+static void add_blend_at(ws2812_array& list, const ws2812_color& color, float pos) {
   int low_pos  = floor(pos);
   int next_pos = (low_pos + 1) % ring_size;
 
   float mix_high = pos - floor(pos);
   float mix_low  = 1.0 - mix_high;
 
-  list.colors[low_pos]  = color_fraction(color, mix_low);
-  list.colors[next_pos] = color_fraction(color, mix_high);
+  /* TODO: this might look nicer with operator overloading */
+  list.colors[low_pos]  = color_add(color_fraction(color, mix_low), list.colors[low_pos]);
+  list.colors[next_pos] = color_add(color_fraction(color, mix_high), list.colors[next_pos]);
+}
+
+ws2812_color hours_color   = { 130,   0,  0 };
+ws2812_color minutes_color = {   0, 110,  0 };
+ws2812_color seconds_color = {   0,   0, 90 };
+
+/* The time positions shown are continuous: the position of hour smoothly
+ * advances 1/12th of the ring during an hour.
+ * To do this, we get the current hour, and subtract the current time from that. We reset
+ * the current hour once an hour :)
+ */
+static bool last_hour_set = false;
+static int current_hour = 0;
+static system_clock::time_point last_hour;
+
+void set_last_hour() {
+  time_t time = system_clock::to_time_t(system_clock::now());
+  tm time_tm;
+
+  localtime_r(&time, &time_tm);
+  time_tm.tm_min = 0;
+  time_tm.tm_sec = 0;
+
+  last_hour = system_clock::from_time_t(mktime(&time_tm));
+  current_hour = time_tm.tm_hour;
+}
+
+void set_colors_for_now(ws2812_array& colors) {
+  set_last_hour();
+  auto ms = duration_cast<milliseconds>(system_clock::now() - last_hour);
+
+  /* Non-monotonic clock ... */
+  if (ms < milliseconds(0))
+    ms = milliseconds(0);
+  /* Update for the next hour */
+  if (ms >= duration_cast<milliseconds>(hours(1))) {
+    set_last_hour();
+    ms = milliseconds(0);
+  }
+
+  /* TODO: this looks not really nice, although the above trickery also doesn't feel well */
+  float hours_pos   = float(current_hour) + float(ms.count()) / 60.0 / 60.0 / 1000.0;
+  float minutes_pos = float(ms.count()) / 60.0 / 1000.0;
+  float seconds_pos = float(ms.count() - 60*1000*floor(minutes_pos)) / 1000.0;
+
+  colors = clean_colors;
+  add_blend_at(colors, hours_color, hours_pos * float(ring_size)/12.0);
+  add_blend_at(colors, minutes_color, minutes_pos * float(ring_size)/60.0);
+  add_blend_at(colors, seconds_color, seconds_pos * float(ring_size)/60.0);
 }
 
 int main(int argc, char** argv) {
@@ -46,17 +115,16 @@ int main(int argc, char** argv) {
   struct ws28128_bcm2708 out = { .buffer = NULL };
   struct ws2812_array colors = clean_colors;
 
-  chrono::milliseconds rest(ms_per_frame);
+  milliseconds rest(ms_per_frame);
 
-  for (int i = 0; i <= frames_per_min; i++) {
-    colors = clean_colors;
-    insert_blended_at(colors, {125,0,0}, float(i) * float(ring_size) / float(frames_per_min) );
+  while(true) {
+    set_colors_for_now(colors);
     output_list(&colors, pwm, &out);
 
     this_thread::sleep_for(rest);
   }
 
-  std::free(out.buffer);
+  free(out.buffer);
   pwm_bwm2708_free(pwm);
 
   return 0;
